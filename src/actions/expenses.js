2303f7541cec16da466a089d56ff1f8395773ec2
Rea Newell <patrick.r.newell@gmail.com>
2018-03-21 16:50:26 +0900

    setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,6 +33,15 @@ export const removeExpense = ({ id } = {}) => ({
     type: "REMOVE_EXPENSE",
     id
 });
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+    };
+};
+
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: "EDIT_EXPENSE",
@@ -45,7 +54,6 @@ export const setExpenses = (expenses) => ({
     type: "SET_EXPENSES",
     expenses
 });
-
 export const startSetExpenses = () => {
     return (dispatch) => {
         return database.ref('expenses').once('value').then((snapshot) => {
